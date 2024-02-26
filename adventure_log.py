1f8d0ae5ae19b1cb24ff1799dd8863b8f1f68bc7
jordynbelli <bellijordyn@gmail.com>
2024-02-26 17:13:20 -0600

    updated python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Hello, coding temple navigators!")
+    print("hello, coding temple navigators!!!")
 
     if __name__-- "__main__":
         greet_coding_temple()
