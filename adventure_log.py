2c29f48e55998a60b6bb37f7d21a151010405c14
jordynbelli <bellijordyn@gmail.com>
2024-03-02 18:13:36 -0600

    second update

--- adventure_log.py
+++ adventure_log.py
@@ -3,8 +3,3 @@ def greet_coding_temple():
 
     if __name__-- "__main__":
         greet_coding_temple()
-def greet_coding_temple():
-    print("Ahoy, coding temple navigators! Ready for today's adventure?")
-
-    if __name__-- "__main__":
-        greet_coding_temple()
