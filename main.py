d734cec5d3a545b5c1a2bc22a5ad88108876cc50
jordynbelli <bellijordyn@gmail.com>
2024-03-02 21:35:30 -0600

    main.py addition

---
+++ main.py
@@ -0,0 +1,6 @@
+favorite_quote = """
+Python Programming is much more than just a skill;
+it is entering the world of creativity, 
+and every code is a step towards mastery
+"""
+print(favorite_quote)
